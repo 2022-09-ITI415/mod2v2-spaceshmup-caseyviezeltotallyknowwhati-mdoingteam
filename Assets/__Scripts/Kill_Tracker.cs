8cd50ac944c4659b526fecd5fa90d901d8115173
GeneralSkeltal <cjv81@scarletmail.rutgers.edu>
2022-10-20 21:30:21 -0400

    fixed the kill counter thing

--- Assets/__Scripts/Kill_Tracker.cs
+++ Assets/__Scripts/Kill_Tracker.cs
@@ -6,25 +6,61 @@ using UnityEngine.UI;
 
 public class Kill_Tracker : MonoBehaviour
 {
+    public int startLevelKillCount = 1;
     public int KillCount;
     public Text Destroyed;
+    public Text levelText;
+    string destroyText;
     public int EnemiesToDestroy;
     // Start is called before the first frame update
     void Start()
     {
         KillCount = 0;
-        Destroyed.text = "Enemies Destroyed: " + KillCount + "/" + EnemiesToDestroy;
+        EnemiesToDestroy = startLevelKillCount;
+        destroyText = "Enemies Destroyed: " + KillCount + "/" + EnemiesToDestroy;
+        Destroyed.text = destroyText;
 
     }
     public void IncreaseKillCount()
     {
         KillCount = KillCount + 1;
+        destroyText = "Enemies Destroyed: " + KillCount + "/" + EnemiesToDestroy;
+        Destroyed.text = destroyText;
         if (KillCount == EnemiesToDestroy)
         {
-            SceneManager.LoadScene("_Scene_0");
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
             KillCount = 0;
             EnemiesToDestroy = EnemiesToDestroy + 5;
+            destroyText = "Enemies Destroyed: " + KillCount + "/" + EnemiesToDestroy;
+            StartCoroutine(NewLevelLoad());
         }
-        Destroyed.text = "Enemies Destroyed: " + KillCount + "/" + EnemiesToDestroy;
+    }
+
+    IEnumerator NewLevelLoad()
+    {
+        //Display Level complete text
+        levelText.gameObject.SetActive(true);
+        levelText.text = "Mission Complete";
+        yield return new WaitForSeconds(1);
+        //Display new level text
+        levelText.text = "New Mission Objective\n" + destroyText;
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+        yield return new WaitForSeconds(2);
+        levelText.gameObject.SetActive(false);
+        Destroyed.text = destroyText;
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject enemy in enemies)
+        {
+            Destroy(enemy);
+        }
+        //Display new enemies to destroy text
     }
 }
